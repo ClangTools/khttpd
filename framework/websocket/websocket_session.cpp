e4effd1edad8adc110b2c3e1099a3afd8fa2cbaa
kekxv <caesar@kekxv.com>
2025-07-06 22:36:39 +0800

    update (#5)
    * fix: ci/cd
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update dep
    
    * update websocket session id
    
    * update websocket session id
    
    * fix: ci/cd
    
    * update
    
    * read_message_max

--- framework/websocket/websocket_session.cpp
+++ framework/websocket/websocket_session.cpp
@@ -21,6 +21,7 @@ namespace khttpd::framework
       std::unique_lock<std::mutex> lock(m_gen_mutex);
       id = boost::uuids::to_string(gen());
     }
+    ws_.read_message_max(32 * 1024 * 1024);
     ws_.set_option(ws::stream_base::decorator([](ws::response_type& res)
     {
       res.set(http::field::server, std::string(BOOST_BEAST_VERSION_STRING) + " khttpd-websocket");
@@ -114,9 +115,49 @@ namespace khttpd::framework
 
   void WebsocketSession::do_write(std::shared_ptr<const std::string> ss, bool is_text_msg)
   {
+    // 设置消息是文本还是二进制
     ws_.text(is_text_msg);
-    ws_.async_write(net::buffer(*ss),
-                    beast::bind_front_handler(&WebsocketSession::on_write, shared_from_this()));
+
+    // --- 检查消息大小，决定是否分片 ---
+    if (ss->length() < auto_fragment_threshold_)
+    {
+      // 消息不大，直接发送，无需分片。
+      // 这可以避免为小消息创建 vector 和 buffer sequence 的开销。
+      ws_.async_write(net::buffer(*ss),
+                      beast::bind_front_handler(&WebsocketSession::on_write, shared_from_this()));
+    }
+    else
+    {
+      // 消息很大，需要分片发送。
+      // 1. 创建一个缓冲区序列（vector of const_buffer）的 shared_ptr。
+      //    必须用 shared_ptr 来管理，因为它需要在异步操作期间保持存活。
+      auto buffer_sequence_ptr = std::make_shared<std::vector<net::const_buffer>>();
+
+      // 2. 预留空间以提高效率
+      buffer_sequence_ptr->reserve(ss->length() / fragment_size_ + 1);
+
+      // 3. 将大字符串切分成多个 buffer，并添加到序列中。
+      //    这个过程不会拷贝字符串数据，net::const_buffer 只是一个视图。
+      size_t offset = 0;
+      while (offset < ss->length())
+      {
+        size_t current_chunk_size = std::min(fragment_size_, ss->length() - offset);
+        buffer_sequence_ptr->emplace_back(ss->data() + offset, current_chunk_size);
+        offset += current_chunk_size;
+      }
+
+      // 4. 调用 async_write，传入缓冲区序列。
+      //    Beast 会自动将序列中的每个 buffer 作为一帧来发送。
+      ws_.async_write(
+        *buffer_sequence_ptr, // 传入缓冲区序列
+        [ss, buffer_sequence_ptr, self = shared_from_this()](beast::error_code ec, std::size_t bytes)
+        {
+          // 这个 lambda 的作用是确保 ss 和 buffer_sequence_ptr 的生命周期
+          // 能够覆盖整个异步写操作。当 on_write 被调用时，它们依然有效。
+          self->on_write(ec, bytes);
+        }
+      );
+    }
   }
 
   void WebsocketSession::on_write(beast::error_code ec, std::size_t bytes_transferred)
